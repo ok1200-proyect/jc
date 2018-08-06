e60296ea8ade12725d66dc0403b6492d93c9e474
Kevin Toro <kctoro74@gmail.com>
2018-08-06 09:52:38 -0500

    Add files via upload

---
+++ js/index.php
@@ -0,0 +1,270 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Juan Carmona - Design studio - Freelance Graphic and Web Designer</title>
+        <meta name="description" content="I am a Freelance Graphic and Web Designer based in Medellín, Colombia. I work remotely helping businesses with: Web Design & Development, App Design, Branding and Social Marketing.">
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Google fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400|Open+Sans:300,400,500" rel="stylesheet">
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108399305-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-108399305-1');
+        </script>
+        <script type="application/ld+json">
+           {
+             "@context": "http://schema.org/",
+             "type": "Person",
+             "ContactPoint": {
+               "type": "ContactPoint",
+               "contactType": "Customer service",
+               "email": "hello@juancarmona.co",
+               "url": "https://www.juancarmona.co"
+             },
+             "description": "I am a Freelance Graphic and Web Designer, I work along with Professionals, Agencies and Companies at projects like Branding and Graphic Design, Responsive Websites and User Interfaces and also Web Development.",
+             "image": "https://www.juancarmona.co/img/juan-profile.jpg",
+             "jobTitle": "Graphic and Web Designer",
+             "name": "Juan Carmona",
+             "url": "https://www.juancarmona.co/",
+             "makesOffer": "Branding, Graphic Design, Responsive Web Design and Development, App Design, Social Marketing."
+           }
+       </script>
+    </head>
+    <body>
+        <!--Menu-->
+        <div class="Contorno-menu">
+            <nav class="Menu-container fixed-top">
+                <div class="container px-0">
+                    <ul class="Menu">
+                      <li class="Menu-item Item_1"><a href="#Header-section" class="Slide">
+                        <img src="img/juancarmona-icon.png"
+                        srcset="img/juancarmona-icon.png 1x, img/juancarmona-icon@2x.png 2x" 
+                        alt="Juan Carmona Icon" class="Menu-logo">
+                      </a></li>
+                      <li class="Menu-item Item_2"><a href="#Contact-section" class="Menu-button Slide Up-animation">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>     
+        </div>
+        <!--Menu-->
+
+        <!--Header-->
+        <div class="Header-section" id="Header-section">
+            <div class="container">
+                <div class="row Header-row">
+                    <section class="col-12 d-flex justify-content-center" data-aos="fade-up" data-aos-delay="100">
+                        <img src="img/Juan-Carmona-Design-Studio.png"
+                        srcset="img/Juan-Carmona-Design-Studio.png 1x, img/Juan-Carmona-Design-Studio@2x.png 2x" 
+                        alt="Juan carmona logo" class="Header-logo d-none d-md-block">
+                        <img src="img/juancarmona-icon.png"
+                        srcset="img/juancarmona-icon.png 1x, img/juancarmona-icon@2x.png 2x" 
+                        alt="Juan Carmona Logo" class="Header-logo Header-logo_2 d-md-none d-block">
+                    </section>
+                    <section class="col-md-8 col-12 Text-header">
+                        <h2 data-aos="fade-up" data-aos-delay="200">Hello, my name is Juan Carmona. </h2>
+                        <h1 data-aos="fade-up" data-aos-delay="300">I am a Freelance Graphic &amp; Web Designer based in Medellín, Colombia.</h1>
+                        <h3 class="mb-0" data-aos="fade-up" data-aos-delay="400">I work with some talented partners to offer professional services focused in:</h3>
+                    </section>
+                    <section class="Services d-none d-md-block">
+                        <h2 class="Keyword" data-aos="fade-right" data-aos-delay="500">Web Design &amp; Development</h2>
+                        <h2 class="Keyword" data-aos="zoom-out" data-aos-delay="600">App Design</h2>
+                        <h2 class="Keyword Keyword-last" data-aos="fade-left" data-aos-delay="700">Branding &amp; Marketing</h2>
+                    </section>
+                    <section class="Services Services-movile d-block d-md-none">
+                        <h2 class="Keyword" data-aos="zoom-out">Web Design & Development,<br> App Design and Branding</h2>
+                    </section>
+                    <a href="#Contact-section" class="Menu-button Header-button Slide Up-animation d-flex d-md-none">Contact</a>
+
+                </div>
+            </div>
+        </div>
+        <!--Header-->
+
+        <!--Projects-->
+        <div class="Projects">
+            <div class="Projects-container">
+                <h3 class="Title Title_projects">Projects</h3> 
+                <div class="row Row-projects"> <!--Gatha-->
+                    <div class="col-xl-8 col-12 Col_1" data-aos="fade-up"> 
+                        <div class="Project-img Gatha-project">
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-8 col-md-10 col-12 Col_2">
+                        <article class="Project-info" data-aos="fade-down"> 
+                            <h4 class="Title Title_project Line_green">Gatha</h4>
+                            <h5 class="Service-project">App Design &amp; Web Development</h5>
+                            <address class="Location-project">Fort Lauderdale, United States.</address>
+                            <p class="Description-project">Gatha is an amazing startup with the mission of making sharing location easier.
+                            </p>
+                            <p class="Description-project mb-0">I am currently working as UI/UX Designer at Gatha. At my work there I am responsible of the App UI Design and the Web Design &amp; Development.</p>
+                        </article>
+                    </div>
+                </div> 
+                <div class="row Row-projects Row-projects_inverse"> <!--JC Branding-->
+                    <div class="col-xl-4 col-lg-8 col-md-10 col-12 Col_2 Col_2-inverse">
+                        <article class="Project-info" data-aos="fade-down"> 
+                            <h4 class="Title Title_project Line_yellow">Juan Carmona</h4>
+                            <h5 class="Service-project">Branding Identity</h5>
+                            <address class="Location-project">Made with passion at Medellín, Colombia.</address>
+                            <p class="Description-project mb-0">This is my personal branding project, I decided to share it here as an example of my Branding Skills.
+                            </p>
+                        </article>
+                    </div>                    
+                    <div class="col-xl-8 col-12 Col_1 Col_1-inverse order-first order-xl-last" data-aos="fade-up"> 
+                        <div class="Project-img JC-project">
+                        </div>
+                    </div>
+                </div> 
+                <div class="row Row-projects"> <!--Sin Fronteras-->
+                    <div class="col-xl-8 col-12 Col_1" data-aos="fade-up"> 
+                        <div class="Project-img SF-project">
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-8 col-md-10 col-12 Col_2">
+                        <article class="Project-info" data-aos="fade-down"> 
+                            <h4 class="Title Title_project Line_orange">Sin Fronteras Fundación</h4>
+                            <h5 class="Service-project">Branding Identity</h5>
+                            <address class="Location-project">Medellín, Colombia.</address>
+                            <p class="Description-project">Sin Fronteras is a Foundation created to educate and support low-income children and families from Medellín City.
+                            </p>
+                            <p class="Description-project mb-0">I worked in redesigning their Brand Identity so they can have better visual impact and achieve their goals as foundation.</p>
+                        </article>
+                    </div>
+                </div> 
+                <div class="row Row-projects Row-projects_inverse"> <!--DCM-->
+                    <div class="col-xl-5 col-lg-8 col-md-10 col-12 Col_2 Col_2-inverse Col-dcm">
+                        <article class="Project-info" data-aos="fade-down"> 
+                            <h4 class="Title Title_project Line_blue">DCM Servicios y Consultoría</h4>
+                            <h5 class="Service-project">Web Design &amp; Development</h5>
+                            <address class="Location-project">Medellín, Colombia.</address>
+                            <p class="Description-project">DCM is a Medellín based company specialized in work at height training courses and SSH Consultancy.
+                            </p>
+                            <p class="Description-project mb-0">I worked along with DCM to make their new Logotype and Website Design and Development. Their website has a special feature where students can check their certifications.
+                            </p>
+                        </article>
+                    </div>                    
+                    <div class="col-xl-7 col-12 Col_1 Col_1-inverse order-first order-xl-last" data-aos="fade-up"> 
+                        <div class="Project-img DCM-project">
+                        </div>
+                    </div>
+                </div>
+                <div class="row Row-projects"> <!--BO Project-->
+                    <div class="col-xl-8 col-12 Col_1" data-aos="fade-up"> 
+                        <div class="Project-img BO-project">
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-8 col-md-10 col-12 Col_2">
+                        <article class="Project-info" data-aos="fade-down"> 
+                            <h4 class="Title Title_project Line_purple">Best One</h4>
+                            <h5 class="Service-project">Web Design</h5>
+                            <address class="Location-project">Cali, Colombia.</address>
+                            <p class="Description-project">Best One is a phone repairing company.
+                            </p>
+                            <p class="Description-project mb-0">At this project I colaborated with a Web Development Agency to design the Best One Website.</p>
+                        </article>
+                    </div>
+                </div>                                                                          
+                <div class="row Row-projects Row-projects_inverse Molle-row"> <!--MP Project-->
+                    <div class="col-xl-4 col-lg-8 col-md-10 col-12 Col_2 Col_2-inverse">
+                        <article class="Project-info" data-aos="fade-down"> 
+                            <h4 class="Title Title_project Line_red">Molle Pizza</h4>
+                            <h5 class="Service-project">Web Design</h5>
+                            <address class="Location-project">Cali, Colombia.</address>
+                            <p class="Description-project">Molle Pizza is a pizza delivery restaurant.
+                            </p>
+                            <p class="Description-project mb-0">I made their new Web Design hand in hand with an agency to give a new look &amp; feel and help users to understand their pizza menu.
+                            </p>
+                        </article>
+                    </div>                    
+                    <div class="col-xl-8 col-12 Col_1 Col_1-inverse order-first order-xl-last" data-aos="fade-up"> 
+                        <div class="Project-img MP-project">
+                        </div>
+                    </div>
+                </div>                                      
+            </div>
+        </div>                
+        <!--Projects-->
+ 
+        <!--Contact-->
+        <footer class="Contact-container" id="Contact-section">
+            <div class="container">
+                <h3 class="Title Title_contact">Contact</h3>
+                <p class="Paragraph-contact">Do you want to design your new brand or to build your company website?</p>
+                <p class="Paragraph-contact">I am available to work hand in hand with you, let’s talk about it.</p>
+                <div class="row">
+
+                    <div class="col-lg-6 col-12 Form">
+                      <?php require 'contact-form.php'; ?>
+                    </div>
+                    <div class="col-lg-5 col-12 offset-0 offset-lg-1 order-first order-lg-last">
+                        <section class="Profile-contact">
+                            <img src="img/Juan-Carmona-Photo.png"
+                            srcset="img/Juan-Carmona-Photo.png 1x, img/Juan-Carmona-Photo@2x.png 2x" 
+                            alt="Juan carmona profile picture" class="Profile-contact_img">
+                            <div class="">
+                                <h4 class="Juan-name_contact">Juan carmona</h4>
+                                <h1 class="Juan-ocupation">Freelance Graphic &amp; Website Designer</h1>
+                            </div>
+                        </section>
+                            <address class="mb-0">
+                                <p class="Prefer-email_contact">You can contact me at:</p>
+                                <a href="mailto:hello@juancarmona.co" class="Mail-contact">hello@juancarmona.co</a>
+                                <p class="Prefer-email_contact">Or fill in the form below.</p>
+                            </address> 
+                            <div class="Social-contact d-none d-md-block">
+                                <p class="Like-mywork_contact">If you like my work, follow me at:</p>
+                                <div class="Redes">
+                                    <a href="https://www.facebook.com/juancarmona.design/" target="_blank"><i class="fab fa-facebook"></i></a>
+                                    <a href="https://www.instagram.com/juancarmona.design/" target="_blank"><i class="fab fa-instagram"></i></a>
+                                    <a href="https://www.linkedin.com/in/juancarmona-design/" target="_blank"><i class="fab fa-linkedin"></i></a>
+                                    <a href="https://dribbble.com/juancarmona" target="_blank"><i class="fab fa-dribbble-square"></i></a>
+                                </div>
+                            </div>                      
+                    </div>
+                    <div class="Social-contact d-md-none d-flex">
+                        <p class="Like-mywork_contact">If you like my work, follow me at:</p>
+                        <div class="Redes">
+                            <a href="https://www.facebook.com/juancarmona.design/" target="_blank"><i class="fab fa-facebook"></i></a>
+                            <a href="https://www.instagram.com/juancarmona.design/" target="_blank"><i class="fab fa-instagram"></i></a>
+                            <a href="https://www.linkedin.com/in/juancarmona-design/" target="_blank"><i class="fab fa-linkedin"></i></a>
+                            <a href="https://dribbble.com/juancarmona" target="_blank"><i class="fab fa-dribbble-square"></i></a>
+                        </div>
+                    </div>                     
+                </div>
+                <div class="More-details_section">
+                    <small class="More-details_contact">More details about my projects are coming soon, stay tuned :)</small>
+                </div>
+            </div>
+                <div class="Copyright">
+                    <p class="Copyright-text">Copyright © 2018 - Juan Carmona Design Studio</p>
+                    
+                </div>            
+            
+        </footer>
+        <!--Contact-->
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">              
+    
+        <!-- Our css -->
+        <link rel="stylesheet" href="css/main.css">            
+        <!-- Fontawesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+        <!-- AOS css -->
+        <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
+        <!-- AOS javascript -->
+        <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <!-- Our javascript -->
+        <script type="text/javascript" src="js/all.js"></script>    
+    </body>
+</html>
